5a35e1525d3721d3111099b7ba03dc63782805a6
Václav Dohnal <vaclav.dohnal@gmail.com>
2018-05-30 22:52:27 +0200

    add class docstring

--- django_numpy_json_encoder/numpy_encoder.py
+++ django_numpy_json_encoder/numpy_encoder.py
@@ -7,10 +7,20 @@ from django.core.serializers.json import DjangoJSONEncoder
 
 class NumpyJSONEncoder(DjangoJSONEncoder):
     """
-    Credits: https://stackoverflow.com/questions/26646362/numpy-array-is-not-json-serializable
+    Subclass of :class:`~DjangoJSONEncoder` [1] that can encode `np.ndarray` as standard list.
+
+
+    Ref:
+
+        [1] https://docs.djangoproject.com/en/2.0/topics/serialization/#djangojsonencoder
+
+    Credits:
+
+        https://stackoverflow.com/questions/26646362/numpy-array-is-not-json-serializable
     """
 
     def default(self, o):
+        # noinspection PyUnresolvedReferences
         if isinstance(o, np.ndarray):
             return o.tolist()
 
