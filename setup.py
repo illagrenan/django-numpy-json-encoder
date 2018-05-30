24462e4195e0ccfc5a753c195d50cac8976082d0
Václav Dohnal <vaclav.dohnal@gmail.com>
2018-05-30 21:47:45 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+
+from setuptools import setup
+
+setup(
+    name='djang-numpy-json-encoder',
+    version='0.1.0',
+    description="""Middleware that compresses response using brotli algorithm.""",
+    long_description=io.open("README.rst", 'r', encoding="utf-8").read(),
+    url='https://github.com/illagrenan/django-numpy-json-encoder',
+    license='MIT',
+    author='Vasek Dohnal',
+    author_email='vaclav.dohnal@gmail.com',
+    packages=['django_numpy_json_encoder'],
+    install_requires=['django', 'numpy'],
+    python_requires='~=3.6',
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
