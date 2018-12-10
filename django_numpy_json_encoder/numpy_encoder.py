cb4a164345ca3c62498f00cd462f342ade80dd84
Václav Dohnal <vaclav.dohnal@gmail.com>
2018-12-10 14:44:42 +0100

    better formatting: add types on separate lines

--- django_numpy_json_encoder/numpy_encoder.py
+++ django_numpy_json_encoder/numpy_encoder.py
@@ -23,9 +23,22 @@ class NumpyJSONEncoder(DjangoJSONEncoder):
     """
 
     def default(self, o):
-        if isinstance(o, (np.int_, np.intc, np.intp, np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64)):
+        if isinstance(o, (np.int_,
+                          np.intc,
+                          np.intp,
+                          np.int8,
+                          np.int16,
+                          np.int32,
+                          np.int64,
+                          np.uint8,
+                          np.uint16,
+                          np.uint32,
+                          np.uint64)):
             return int(o)
-        elif isinstance(o, (np.float_, np.float16, np.float32, np.float64)):
+        elif isinstance(o, (np.float_,
+                            np.float16,
+                            np.float32,
+                            np.float64)):
             return float(o)
         # noinspection PyUnresolvedReferences
         elif isinstance(o, np.ndarray):
